63c1a4d7e3e2417d3ce9207d3e6e936f66ad8245
Will Elson <elson594@gmail.com>
2023-07-25 19:43:32 +0100

    Save checked status in local storage

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -4,15 +4,21 @@
       style="display: flex; justify-content: space-between; align-items: center"
     >
       <div class="page-title">Shopping List</div>
-      <span
-        @click="toggleEditing"
-        style="color: var(--primary); margin-right: var(--padding)"
-        >{{ editing ? 'done' : 'edit' }}</span
-      >
+      <div>
+        <span v-if="editing" class="clear-all-btn" @click="clearShoppingList">
+          clear all</span
+        >
+        <span @click="toggleEditing" class="edit-btn">{{
+          editing ? 'done' : 'edit'
+        }}</span>
+      </div>
     </div>
     <div class="list-container">
       <div class="list-item" v-for="item in items" :key="item.id">
-        <div class="checkbox" @click="item.checked = !item.checked">
+        <div
+          class="checkbox"
+          @click="() => updateListItem(item.id, { checked: !item.checked })"
+        >
           <i
             v-if="item.checked"
             class="fa fa-check-circle"
@@ -22,8 +28,10 @@
         </div>
         <input
           type="text"
-          @keyup.enter="(e) => updateItem(e, item.id)"
-          @blur="(e) => updateItem(e, item.id)"
+          @keyup.enter="
+            (e) => updateListItem(item.id, { text: e.target.value })
+          "
+          @blur="(e) => updateListItem(item.id, { text: e.target.value })"
           @focus="editing = false"
           :value="item.text"
           placeholder="New item..."
@@ -31,7 +39,7 @@
         <i
           v-if="editing"
           class="fa fa-times"
-          style="color: var(--secondary); cursor: pointer"
+          style="color: var(--danger); cursor: pointer"
           aria-hidden="true"
           @click="removeEntry(item)"
         ></i>
@@ -81,15 +89,15 @@ export default {
       if (!this.input) return;
       this.addItem();
     },
-    updateItem(event, id) {
+    updateListItem(id, changes) {
       const itemToUpdate = JSON.parse(
         JSON.stringify(this.items.find((i) => i.id === id))
       );
-      if (event.target.value) {
-        itemToUpdate.text = event.target.value;
-        updateItem(SHOPPING_LIST, itemToUpdate);
-      } else {
+
+      if (changes?.text === '') {
         removeItem(SHOPPING_LIST, itemToUpdate);
+      } else {
+        updateItem(SHOPPING_LIST, { ...itemToUpdate, ...changes });
       }
       this.items = getAll(SHOPPING_LIST);
     },
@@ -99,6 +107,11 @@ export default {
     removeEntry(item) {
       removeItem(SHOPPING_LIST, item);
       this.items = getAll(SHOPPING_LIST);
+    },
+    clearShoppingList() {
+      this.items = [];
+      localStorage.setItem(SHOPPING_LIST, []);
+      this.editing = false;
     },
   },
   mounted() {
@@ -143,5 +156,15 @@ export default {
   padding: var(--padding-sm) 0 var(--padding-xs) 0;
   border: none;
   outline: none;
+}
+
+.edit-btn {
+  color: var(--primary);
+  margin: 0 var(--padding-sm);
+}
+.clear-all-btn {
+  color: var(--primary);
+  padding-right: var(--padding-sm);
+  border-right: 1px solid var(--border);
 }
 </style>
